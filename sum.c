1c0edad3870a6172276ca2d6ef0f917360255087
mdaybhaq <mdaybhaq@gmail.com>
2026-06-15 00:28:27 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	int c;
+	int sum=0;
+	printf("Enter first number:\n");
+	scanf("%d", &a);
+	printf("Enter second number:\n");
+	scanf("%d", &b);
+	sum=a+b;
+	printf("Sum is %d",sum);
+	return 0;
+}
